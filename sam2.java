8b714f59f141e55b6314b97bef795abad7171366
samnihal <spuvvada@deloitte.com>
2019-09-24 15:39:07 +0530

    this is second commit

---
+++ sam2.java
@@ -0,0 +1,26 @@
+bin/
+cmd/
+dev/
+etc/
+git-bash.exe*
+git-cmd.exe*
+LICENSE.txt
+mingw64/
+proc/
+ReleaseNotes.html
+tmp/
+unins001.dat
+unins001.exe*
+unins001.msg
+this is for teting with sam 
+:wq
+this is for additional content to check checkpoint for datd
+usr/:wq
+wq
+:wq
+:wq
+:wq
+nkkk
+sjjndasjndak
+knakldjnaskjdnasjk
+thiws is second editing
